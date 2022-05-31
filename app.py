83ddd13cfaecdc69f9445cbf89587656addc879d
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-05-31 17:36:30 +0200

    Rebuilds the fundamental routes and templates for Flask

--- app.py
+++ app.py
@@ -4,5 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template('page/home.html')
+
+@app.route("/openapi")
+def openapi():
+    return render_template('redoc/index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
