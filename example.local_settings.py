ba42169f2c607fbeb13cee55f4da9ded9a312257
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-06-02 14:32:15 +0200

    Includes the Excel headers mapped to API keys

--- example.local_settings.py
+++ example.local_settings.py
@@ -9,3 +9,19 @@ data_dir = 'data'
 
 # ECHE list source file (Excel).
 eche_xlsx = 'accredited-heis-erasmus-2021-2027-mar22_en.xlsx'
+
+# ECHE list headers and corresponding API keys.
+eche_headers = {
+    'Proposal Number': 'proposalNumber',
+    'Erasmus Code': 'erasmusCode',
+    'PIC': 'pic',
+    'OID': 'oid',
+    'Organisation Legal Name': 'organisationLegalName',
+    'Street': 'street',
+    'Postal Code': 'postalCode',
+    'City': 'city',
+    'Country': 'country',
+    'Webpage': 'webpage',
+    'ECHE Start Date': 'echeStartDate',
+    'ECHE End Date': 'echeEndDate'
+}
