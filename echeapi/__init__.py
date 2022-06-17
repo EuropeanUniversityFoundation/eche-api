bd464d97f74ad77f9804f6b21fefc3c86985ffd0
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-06-17 16:10:18 +0200

    Adds a logger to the Flask app

--- echeapi/__init__.py
+++ echeapi/__init__.py
@@ -1,8 +1,13 @@
 
+from logging import FileHandler, WARNING
+
 from flask import Flask
 from jinja_markdown import MarkdownExtension
 
 from echeapi import settings
+
+file_handler = FileHandler('app.log')
+file_handler.setLevel(WARNING)
 
 app = Flask(
     import_name=__name__,
@@ -11,5 +16,6 @@ app = Flask(
 )
 app.jinja_env.add_extension(MarkdownExtension)
 app.secret_key = settings.app_secret_key
+app.logger.addHandler(file_handler)
 
 import echeapi.interface.views
