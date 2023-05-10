5c92bbb356c97dde0ad9d674910cb113f666a110
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-05-10 11:28:16 +0200

    Adds support for alternative port numbers in Flask

--- echeapi/scripts/run.py
+++ echeapi/scripts/run.py
@@ -5,5 +5,11 @@ from echeapi import app
 
 
 def main(*args):
+    port_number = 5000
+
+    for i, arg in enumerate(args):
+        if arg in ('-p', '--port'):
+            port_number = args[i + 1]
+
     os.environ.setdefault('FLASK_ENV', 'development')
-    app.run(debug=True)
+    app.run(debug=True, port=port_number)
